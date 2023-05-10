46f2e365b4b9b00ed09e1d9ea48e9c2cc79b6b71
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-05-10 19:40:40 +0000

    fix

--- monty.c
+++ monty.c
@@ -10,18 +10,18 @@ int stack_ptr = 0;
 void push(char *arg, int line_num) {
   if (arg == NULL || *arg == '\0') {
     fprintf(stderr, "L%d: usage: push integer\n", line_num);
-    return;
+    exit(EXIT_FAILURE);
   }
 
   int val = atoi(arg);
-  if (val == 0 && arg[0] != '0') {
+  if (val == 0 && *arg != '0') {
     fprintf(stderr, "L%d: usage: push integer\n", line_num);
-    return;
+    exit(EXIT_FAILURE);
   }
 
   if (stack_ptr >= STACK_SIZE) {
     fprintf(stderr, "L%d: error: stack overflow\n", line_num);
-    return;
+    exit(EXIT_FAILURE);
   }
 
   stack[stack_ptr++] = val;
