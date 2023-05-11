98a85df6c3990bba82fb981bff8757888f6c8492
Ahmed Toukebri <35146059+Touka01@users.noreply.github.com>
2023-05-11 16:20:01 +0000

    monty

---
+++ monty.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "monty.h"
+
+stack_t *stack = NULL; /* Global variable representing the stack */
+
+void push(stack_t **stack, int value);
+void pall(stack_t **stack);
+
+int main(int argc, char *argv[]) {
+    /* Check if the correct number of arguments is provided */
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    /* Open the file */
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    /* Read the file line by line */
+    char *line = NULL;
+    size_t line_size = 0;
+    ptrdiff_t read;
+
+    unsigned int line_number = 1;
+
+    while ((read = getline(&line, &line_size, file)) != -1) {
+        /* Process each line of the file */
+
+        /* Parse the opcode and argument from the line */
+        /* Assuming the opcode and argument are separated by a space */
+        char *opcode = strtok(line, " \n\t");
+        char *arg = strtok(NULL, " \n\t");
+
+        if (opcode != NULL) {
+            /* Handle push opcode */
+            if (strcmp(opcode, "push") == 0) {
+                /* Check if an argument is provided */
+                if (arg == NULL) {
+                    fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                    fclose(file);
+                    free(line);
+                    return EXIT_FAILURE;
+                }
+                /* Convert the argument to an integer using atoi */
+                int value = atoi(arg);
+                /* Push the value to the stack */
+                push(&stack, value);
+            }
+            /* Handle pall opcode */
+            else if (strcmp(opcode, "pall") == 0) {
+                /* Print all the values on the stack */
+                pall(&stack);
+            }
+            /* Handle other opcodes... */
+        }
+
+        line_number++;
+    }
+
+    fclose(file);
+    free(line);
+    return EXIT_SUCCESS;
+}
+
+void push(stack_t **stack, int value) {
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL) {
+        (*stack)->prev = new_node;
+    }
+
+    *stack = new_node;
+}
+
+void pall(stack_t **stack) {
+    stack_t *current = *stack;
+
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
